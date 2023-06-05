20ebe4a3ae727b47d974483feebcc4addd40ad32
AkashDungrani <www.akashdungrani170@gmail.com>
2023-06-05 13:41:13 +0530

    first commit

---
+++ lib/view/pages/homepage.dart
@@ -0,0 +1,339 @@
+
+import 'dart:math';
+
+import 'package:date_picker_timeline/date_picker_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+import 'package:to_do_app/view/pages/add_task_bar.dart';
+import '../../db/db_helper.dart';
+import '../../globle/theme.dart';
+import 'button.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  DateTime _selectedDate = DateTime.now();
+  Future<List<Map<String, Object?>>?>? data;
+  int item = 0;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getdata();
+  }
+
+  getdata() async {
+    data = DBHelper.dbHelper
+        .selectAll(date: DateFormat.yMd().format(DateTime.now()).toString());
+    setState(() {});
+  }
+
+  Stream<DateTime> getTime() async* {
+    DateTime currentTime = DateTime.now();
+    while (true) {
+      await Future.delayed(Duration(seconds: 1));
+      yield currentTime;
+      setState(() {});
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      appBar: _appBar(),
+      body: Column(
+        children: [
+          _addTaskBar(),
+          _addDateBar(),
+          Expanded(
+            child: FutureBuilder(
+              future: data,
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text("${snapshot.error}"),
+                  );
+                } else if (snapshot.hasData) {
+                  List<Map<String, Object?>>? data1 = snapshot.data;
+
+                  return ListView.builder(
+                    itemCount: data1!.length,
+                    itemBuilder: (context, index) {
+                      return Column(
+                        children: [
+                          StreamBuilder(
+                            stream: getTime(),
+                            builder: (context, snapshot) {
+                              if (snapshot.hasError) {
+                              } else if (snapshot.hasData) {
+                                DateTime? data2 = snapshot.data;
+
+                                print(TimeOfDay(
+                                        hour: data2!.hour, minute: data2.minute)
+                                    .format(context)
+                                    .toString());
+
+                                if (TimeOfDay(
+                                                hour: data2.hour,
+                                                minute: data2.minute)
+                                            .format(context)
+                                            .toString() ==
+                                        data1[index]['endTime'] &&
+                                    item == 0) {
+                                  Future.delayed(
+                                    Duration(seconds: 1),
+                                    () async {
+                                      await DBHelper.dbHelper
+                                          .DeleteAll(id: index + 1);
+                                      item == 1;
+                                      setState(() {
+                                        data = DBHelper.dbHelper.selectAll(
+                                            date: DateFormat.yMd()
+                                                .format(_selectedDate)
+                                                .toString());
+                                      });
+                                    },
+                                  );
+                                }
+                              }
+                              return Container();
+                            },
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              padding: EdgeInsets.symmetric(vertical: 10),
+                              margin: EdgeInsets.symmetric(horizontal: 10),
+                              decoration: BoxDecoration(
+                                color: (data1[index]['color'] == 0)
+                                    ? bluisClr
+                                    : (data1[index]['color'] == 1)
+                                        ? pinkClr
+                                        : yellowClr,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Container(
+                                margin: EdgeInsets.symmetric(horizontal: 10),
+                                child: Row(
+                                  children: [
+                                    Expanded(
+                                      flex: 7,
+                                      child: Container(
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              "${data1[index]['title']}",
+                                              style: TextStyle(
+                                                fontSize: 16,
+                                                fontWeight: FontWeight.w600,
+                                                color: Colors.white,
+                                              ),
+                                            ),
+                                            SizedBox(
+                                              height: 5,
+                                            ),
+                                            Row(
+                                              children: [
+                                                Icon(
+                                                  Icons.access_time,
+                                                  color: Colors.white,
+                                                  size: 20,
+                                                ),
+                                                Text(
+                                                  " ${data1[index]['startTime']} - ",
+                                                  style: TextStyle(
+                                                    fontSize: 12,
+                                                    color: Colors.white,
+                                                  ),
+                                                ),
+                                                Text(
+                                                  "${data1[index]['endTime']}",
+                                                  style: TextStyle(
+                                                    fontSize: 12,
+                                                    color: Colors.white,
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                            SizedBox(
+                                              height: 7,
+                                            ),
+                                            Text(
+                                              "${data1[index]['note']}",
+                                              style: TextStyle(
+                                                fontSize: 14,
+                                                fontWeight: FontWeight.w500,
+                                                color: Colors.white,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                    // SizedBox(
+                                    //   width: 10,
+                                    // ),
+                                    Expanded(
+                                      child: Container(
+                                        height: 80,
+                                        child: Row(
+                                          children: [
+                                            Container(
+                                              height: 80,
+                                              width: 0.5,
+                                              color: Colors.white,
+                                            ),
+                                            Transform.rotate(
+                                                angle: pi / 2,
+                                                child: Text(
+                                                  "TODO",
+                                                  style: TextStyle(
+                                                    fontSize: 12,
+                                                    height: 3,
+                                                    color: Colors.white,
+                                                  ),
+                                                ))
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                }
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _addDateBar() {
+    return Container(
+      margin: EdgeInsets.only(top: 20, left: 20),
+      child: DatePicker(
+        DateTime.now(),
+        height: 100,
+        width: 80,
+        initialSelectedDate: DateTime.now(),
+        selectionColor: primaryClr,
+        selectedTextColor: Colors.white,
+        dateTextStyle: GoogleFonts.lato(
+          textStyle: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+            color: Colors.grey,
+          ),
+        ),
+        dayTextStyle: GoogleFonts.lato(
+          textStyle: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+            color: Colors.grey,
+          ),
+        ),
+        monthTextStyle: GoogleFonts.lato(
+          textStyle: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w600,
+            color: Colors.grey,
+          ),
+        ),
+        onDateChange: (selectedDate) {
+          _selectedDate = selectedDate;
+          setState(() {
+            data = DBHelper.dbHelper.selectAll(
+                date: DateFormat.yMd().format(_selectedDate).toString());
+          });
+        },
+      ),
+    );
+  }
+
+  _addTaskBar() {
+    return Container(
+      margin: EdgeInsets.only(left: 20, right: 20, top: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  DateFormat.yMMMMd().format(DateTime.now()),
+                  style: subHeading,
+                ),
+                Text(
+                  "Today",
+                  style: Heading,
+                )
+              ],
+            ),
+          ),
+          ButtonPage(
+              onTap: () async {
+                await Get.to(
+                  AddTaskPage(),
+                );
+                setState(() {
+                  data = DBHelper.dbHelper.selectAll(
+                      date: DateFormat.yMd().format(_selectedDate).toString());
+                });
+              },
+              label: "+ Add Task"),
+        ],
+      ),
+    );
+  }
+
+  _appBar() {
+    return AppBar(
+      elevation: 0,
+      leading: GestureDetector(
+        onTap: () {
+          if (Get.isDarkMode) {
+            Get.changeThemeMode(ThemeMode.light);
+          } else {
+            Get.changeThemeMode(ThemeMode.dark);
+          }
+        },
+        child: Icon(
+          (Get.isDarkMode) ? Icons.wb_sunny_outlined : Icons.nightlight_round,
+          size: 20,
+          color: (Get.isDarkMode) ? Colors.white : Colors.black,
+        ),
+      ),
+      actions: [
+        CircleAvatar(
+          backgroundColor: (Get.isDarkMode) ? Colors.white : Colors.black,
+          child: Icon(
+            Icons.person,
+            color: Theme.of(context).backgroundColor,
+          ),
+        ),
+      ],
+      backgroundColor: Theme.of(context).backgroundColor,
+    );
+  }
+}
